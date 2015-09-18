b7937d38aaf6c457d1943d48bbea310354bbfc37
josemq2000 <josemq2000@gmail.com>
2015-09-18 14:22:37 -0600

    Other visual changes

--- content/apps/_template/src.js
+++ content/apps/_template/src.js
@@ -1,15 +1,10 @@
-
-var id_usuario 	  	 = $(userInfo.getUserInfo())[0]['userid'];
-var nombre_completo  = $(userInfo.getUserInfo())[0]['user_displayname']; 										 
-var departamento	 = $(userInfo.getUserInfo())[0]['user_department'];
-var correo			 = $(userInfo.getUserInfo())[0]['user_email'];
-var cargo		 	 = $(userInfo.getUserInfo())[0]['user_title'];
+var uid 	= $(userInfo.getUserInfo())[0]['userid'];
+var uname  	= $(userInfo.getUserInfo())[0]['user_displayname'];
+var email	= $(userInfo.getUserInfo())[0]['user_email'];
 
 $(function(){
-	 var info = "<p><b>User id</b>: " + id_usuario + "</p>" +
-	 	 	  	"<p><b>User name</b>: " + nombre_completo + "</p>" +
-				"<p><b>Department</b>: " + departamento + "</p>" +
-				"<p><b>E-mail</b>: " + correo + "</p>" +
-				"<p><b>Title</b>: " + cargo + "</p>";
-	 $("#USER_INFO").html(info);	
+	 var info = "<p><b>User id</b>: " + uid + "</p>" +
+	 	 	  	"<p><b>User name</b>: " + uname + "</p>" +
+				"<p><b>E-mail</b>: " + email + "</p>";
+	 $("#USER_INFO").html(info);
 });
--- content/apps/logs/index.php
+++ content/apps/logs/index.php
@@ -8,7 +8,7 @@
 <p><span><?php echo APPLANG_LOGS_DESCRIPTION; ?></span></p>
 <div id="DIVSEARCH">	 
 	 <input type="button" class="_INPUT_BUTTON" onclick="showLogs()" value="<?php echo APPLANG_LOGS_REFRESHBT; ?>" style="float:left;" />	 
-	 <label class="INPUT_LABEL"><?php echo APPLANG_LOGS_SEARCHBT; ?>&nbsp;</label><input type="text" id="LISTSEARCH" class="INPUT_TEXT" />	 	 
+	 <input type="text" placeholder="<?php echo APPLANG_LOGS_SEARCHBT; ?>" id="LISTSEARCH" class="INPUT_TEXT" />	 	 
 </div>
 <hr />
 <p><div class="_CHECKBOX" style="float:left;"><input type="checkbox" id="SHOWONLINE" /><label for="SHOWONLINE"><?php echo APPLANG_LOGS_SHOWONLINECHKBX; ?></label></div></p>
